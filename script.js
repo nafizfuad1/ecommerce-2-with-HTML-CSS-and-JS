33bd7d557963653a28b0ad295e578ea183851979
nafizfuad1 <fuad@echologyx.com>
2026-02-19 21:54:08 +0600

    product end section bar

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const toggleButton = document.getElementById("toggleButton");
 const slidebar = document.getElementById("slidebar");
 
-toggleButton.addEventListener('click',()=>{
-    slidebar.classList.toggle('active');
-})
+toggleButton.addEventListener("click", () => {
+  slidebar.classList.toggle("active");
+});
+
+
+const tabs = document.querySelectorAll(".slide-text");
+const bar = document.querySelector(".option-slide-bar-child");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // active text
+    tabs.forEach((t) => t.classList.remove("active"));
+    tab.classList.add("active");
+
+    // move bar
+    if (tab.dataset.tab === "right") {
+      bar.classList.add("right");
+    } else {
+      bar.classList.remove("right");
+    }
+  });
+});
