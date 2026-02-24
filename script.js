9455b177c7ef2ba18b4487adeb9c1206039725b0
nafizfuad1 <fuad@echologyx.com>
2026-02-24 15:32:21 +0600

    footer added

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ toggleButton.addEventListener("click", () => {
   slidebar.classList.toggle("active");
 });
 
-
 const tabs = document.querySelectorAll(".slide-text");
 const bar = document.querySelector(".option-slide-bar-child");
 
@@ -21,5 +20,51 @@ tabs.forEach((tab) => {
     } else {
       bar.classList.remove("right");
     }
+  });
+});
+
+// PDP slider
+const initSlider = () => {
+  const imageList = document.querySelector(".slide-wrapper .card-list");
+  const slideButtons = document.querySelectorAll(".slider-button");
+
+  const sliderScrollBar = document.querySelector(".slider-scrollbar");
+  const sliderScrollThumb = sliderScrollBar.querySelector(".scrollbar-thumb");
+
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = imageList.clientWidth * direction;
+
+      imageList.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth",
+      });
+    });
+  });
+
+  const updateScrollThumbPosition = () => {
+    const scrollPosition = imageList.scrollLeft;
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    const thumbPosition =
+      (scrollPosition / maxScrollLeft) *
+      (sliderScrollBar.clientWidth - sliderScrollThumb.offsetWidth);
+
+    sliderScrollThumb.style.left = `${thumbPosition}px`;
+  };
+
+  imageList.addEventListener("scroll", updateScrollThumbPosition);
+};
+
+window.addEventListener("load", initSlider);
+
+// mobile footer
+const plusIcons = document.querySelectorAll(".plus-icon");
+
+plusIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const list = icon.parentElement.nextElementSibling.querySelector(".mobile-footer-list");
+    list.classList.toggle("active");
   });
 });
